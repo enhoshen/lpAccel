39322f41ac3b948027fd30056d5f22e54af99421
enhoshen <enhoshen@gmail.com>
2017-10-03 16:21:02 +0800

    IPAD port to interface modification

--- src/PE/PAD.sv
+++ src/PE/PAD.sv
@@ -2,6 +2,99 @@
 // check IF/typedef.sv
 typedef enum logic [3:0]{ IDLE = 0 , WAHEAD= 1 , RAHEAD = 2 , WAITR =3, WAITW = 4
                         }  PixState ;  // pix r/w address overlapping handling
+interface IPcont_if#(              // to and from PE ,
+    parameter ConfDWd = 4,
+    parameter PConfDWd = 3
+);
+    logic [ConfDWd-1:0]  IFLen;   // RxPch  
+    logic [ConfDWd-1:0]  PopU;    // (U-1)Pch+1 
+    logic [PConfDWd-1:0] Pch;    // channel tile Pch
+
+    logic pop ;                 // WPAD tell PE end of Tm, PE tell IPAD to pop
+    logic nxtRow;               // PE keeps track of Tw, swap IF rows
+    
+    logic stall;
+    logic start;
+    logic reset;
+    logic done;
+
+    modport pad(
+        input IFLen,
+        input PopU,
+        input Pch,
+        input pop,
+        input nxtRow,
+        input stall,
+        input start,
+        input reset,
+        input done
+    );
+    modport pe(
+        output IFLen,
+	output PopU,
+	output Pch,
+	output pop,
+	output nxtRow,
+	output stall,
+	output start,
+        output reset,
+	output done
+    );    
+
+endinterface 
+
+interface IPflag_if ;
+    logic zero  ;  // next pixel is zero, posedge
+    logic flag_n;  // negedge trigger , follow zero
+
+    modport ipad(
+        output zero,
+	output flag_n
+    );
+
+    modport wpad(
+        input flag_n
+    );
+
+    modport xbu(
+        input zero
+    );
+
+endinterface
+
+interface IPBpix_if #(  // input pad,buffer pixel interface
+    parameter DWd = 16
+);
+    logic [DWd-1 :0 ] data;
+    logic ready;
+    logic valid; 
+    logic zero ;
+
+    modport pad(   
+        input data,
+	output ready,
+	input valid,
+	input zero
+    );
+    modport ibuf(
+        output data,
+	input ready,
+	output valid,
+	output zero
+    );
+endinterface
+
+interface IPout_if #(  // input pad out pixel interface
+    parameter DWd = 16
+);
+    logic [DWd-1:0] data;
+    logic ready;
+    logic valid;
+    
+
+    modport 
+endinterface
+
 module IFPAD #( 
     parameter DWd =16,
     parameter PadSize = 12,
@@ -12,22 +105,10 @@ module IFPAD #(
 )(
 input i_clk,
 input i_rstn,
-input [ConfDWd-1:0]  i_IFLen,   // RxPch  
-input [ConfDWd-1:0]  i_PopU,    // (U-1)Pch+1 
-input [PConfDWd-1:0] i_Pch,     // channel tile Pch
-input i_pop,                  // finish all the filters and all input channel, pop a data point
-input i_nxtRow,               // next row stage
-
-input  i_WFlagNxt,              // zero flag from WPAD
-output o_IFFlagNxt,            // zero flag to WPAD
-
-input  i_stall,
-input  i_start,
-input  i_reset,
-input  i_done,
-
-sim_Dif.in   i_pix,              // from IFbuffer
-sim_Dif.out  o_pix               // to XBunit  
+IPflag_if.ipad flag,// zero flag to WPAD
+IPcont_if.pad  cont,
+IPBpix_if.pad  ipix,              // from IFbuffer
+IPout_if.pad   opix               // to XBunit  
 );
     //=========================================
     //parameters
@@ -39,14 +120,18 @@ sim_Dif.out  o_pix               // to XBunit
     //=========================================
         // control
     wire en ;
-    PixState pstate_r, pstate_w ; 
+    PixState pstate, pstate_nxt ; 
         // addr
     logic [AddrWd-1:0] waddr_r ,     waddr_w    ;
-    logic [AddrWd-1:0] cur_addr_r ,  cur_addr_w ;
+    logic [AddrWd-1:0] raddr_r ,     raddr_w ;
     logic [AddrWd-1:0] base_addr_r,  base_addr_w;
     
     logic [PadSize-1:0] flag_reg_r, flag_reg_w;
-    logic cur_flag_r , cur_flag_w;
+    logic cur_flag_neg_r   ;   //negedge
+        wire zero = flag_reg_r [waddr_r];  
+
+    logic i_pix_ready_r , i_pix_ready_w;
+    logic o_pix_valid_r , o_pix_valid_w;
     //=========================================
     //RF 12x16b
     //=========================================
@@ -58,21 +143,27 @@ sim_Dif.out  o_pix               // to XBunit
 	    //control 
 	     
     always_comb begin
-        pstate_w     = pstate_r     ;	
+        pstate_nxt     = pstate     ;	
         
 	waddr_w      = waddr_r      ;
-        cur_addr_w   = cur_addr_r   ;
+        raddr_w      = raddr_r   ;
         base_addr_w  = base_addr_r  ;
         
 	flag_reg_w   = flag_reg_r   ; 
-        cur_flag_w   = cur_flag_r   ;	
         
 	case ( pstate_r ) begin
 	    IDLE  :begin
+	    pstate_w = ( )?
+                waddr_w = 0;
+		raddr_w = 0;
+                base_addr_w = 0;
+
+                flag_reg_w = 0;
                 
             end
 	    WAHEAD:begin
-            end
+                o_pix_valid_w = 1'b1;
+	    end
 	    RAHEAD:begin
             end
 	    WAITR :begin
